2db5b19a313fb01ee781d30b1eb62f1a38249cd9
burnsqc <jonathan.r.mobley@gmail.com>
2024-10-20 09:58:14 -0600

    MasterSetupExecutor cont.

--- src/main/java/com/tlregen/api/setup/ClientModEventListeners.java
+++ src/main/java/com/tlregen/api/setup/ClientModEventListeners.java
@@ -1,6 +1,8 @@
 package com.tlregen.api.setup;
 
+import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
 import java.util.function.Supplier;
 
 import org.apache.logging.log4j.LogManager;
@@ -8,21 +10,63 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
+import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.tlregen.util.TextUtil;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.model.SkullModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.particle.ParticleEngine;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderBuffers;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SkullBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.ModelEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.client.event.RegisterDimensionSpecialEffectsEvent;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 public class ClientModEventListeners {
 	protected String modMarker;
 	protected static final Logger LOGGER = LogManager.getLogger("TLREGEN");
 	protected static final Marker SETUP = MarkerManager.getMarker("SETUP");
 
-	Supplier<Map<SkullBlock.Type, ModelLayerLocation>> skullRenderers;
+	Supplier<Map<SkullBlock.Type, ModelLayerLocation>> skullModels;
+	Supplier<Map<ModelLayerLocation, LayerDefinition>> layerDefinitions;
+	Supplier<Map<EntityType<? extends Entity>, EntityRendererProvider<Entity>>> entityRenderers;
+	Supplier<Map<BlockEntityType<? extends BlockEntity>, BlockEntityRendererProvider<BlockEntity>>> blockEntityRenderers;
+	Supplier<List<ResourceLocation>> additionalModels;
+	Supplier<Map<Integer, Block>> blockColorHandlers;
+	Supplier<Map<ResourceLocation, DimensionSpecialEffects>> dimensionSpecialEffects;
+	Supplier<Map<ParticleType<ParticleOptions>, ParticleProvider.Sprite<ParticleOptions>>> particleSprites;
+	Supplier<Map<ParticleType<ParticleOptions>, ParticleEngine.SpriteParticleRegistration<ParticleOptions>>> particleSpriteSets;
+	Supplier<Map<MenuType<? extends AbstractContainerMenu>, MenuScreens.ScreenConstructor<AbstractContainerMenu, ?>>> screens;
+	Supplier<Map<SkullBlock.Type, ResourceLocation>> skullTextures;
+	Supplier<Map<Fluid, RenderType>> fluidRenderTypes;
+	Supplier<SortedMap<RenderType, BufferBuilder>> renderTypes;
 
 	ClientModEventListeners(String modid) {
 		modMarker = "(" + TextUtil.stringToAllCapsName(modid) + ")";
@@ -31,7 +75,61 @@ public class ClientModEventListeners {
 
 	@SubscribeEvent
 	public final void onEntityRenderersEvent$CreateSkullModels(final EntityRenderersEvent.CreateSkullModels event) {
-		skullRenderers.get().forEach((k, v) -> event.registerSkullModel(k, new SkullModel(event.getEntityModelSet().bakeLayer(v))));
-		LOGGER.info(SETUP, modMarker + " SKULLS REGISTERED ");
+		skullModels.get().forEach((k, v) -> event.registerSkullModel(k, new SkullModel(event.getEntityModelSet().bakeLayer(v))));
+		LOGGER.info(SETUP, modMarker + " SKULL MODELS CREATED ");
+	}
+
+	@SubscribeEvent
+	public final void onEntityRenderersEvent$RegisterLayerDefinitions(final EntityRenderersEvent.RegisterLayerDefinitions event) {
+		layerDefinitions.get().forEach((k, v) -> event.registerLayerDefinition(k, () -> v));
+		LOGGER.info(SETUP, modMarker + " LAYER DEFINITIONS REGISTERED ");
+	}
+
+	@SubscribeEvent
+	public final void onEntityRenderersEvent$RegisterRenderers(final EntityRenderersEvent.RegisterRenderers event) {
+		entityRenderers.get().forEach((k, v) -> event.registerEntityRenderer(k, v));
+		LOGGER.info(SETUP, modMarker + " ENTITY RENDERERS REGISTERED ");
+		blockEntityRenderers.get().forEach((k, v) -> event.registerBlockEntityRenderer(k, v));
+		LOGGER.info(SETUP, modMarker + " BLOCK ENTITY RENDERERS REGISTERED ");
+	}
+
+	@SubscribeEvent
+	public final void onModelEvent$RegisterAdditional(final ModelEvent.RegisterAdditional event) {
+		additionalModels.get().forEach((model) -> event.register(model));
+		LOGGER.info(SETUP, modMarker + " ADDITIONAL MODELS REGISTERED ");
+	}
+
+	@SubscribeEvent
+	public final void onRegisterColorHandlersEvent$Block(final RegisterColorHandlersEvent.Block event) {
+		blockColorHandlers.get().forEach((k, v) -> event.register((blockState, blockAndTintGetter, blockPos, tintIndex) -> blockAndTintGetter != null && blockPos != null ? k : -1, v));
+		LOGGER.info(SETUP, modMarker + " BLOCK COLOR HANDLERS REGISTERED ");
+	}
+
+	@SubscribeEvent
+	public final void onRegisterDimensionSpecialEffectsEvent(final RegisterDimensionSpecialEffectsEvent event) {
+		dimensionSpecialEffects.get().forEach((k, v) -> event.register(k, v));
+		LOGGER.info(SETUP, modMarker + " DIMENSION SPECIAL EFFECTS REGISTERED ");
+	}
+
+	@SubscribeEvent
+	public final void onRegisterParticleProvidersEvent(final RegisterParticleProvidersEvent event) {
+		particleSprites.get().forEach((k, v) -> event.registerSprite(k, v));
+		particleSpriteSets.get().forEach((k, v) -> event.registerSpriteSet(k, v));
+		LOGGER.info(SETUP, modMarker + " PARTICLE PROVIDERS REGISTERED ");
+	}
+
+	@SubscribeEvent
+	public final void onFMLClientSetupEvent(final FMLClientSetupEvent event) {
+		event.enqueueWork(() -> {
+			screens.get().forEach((k, v) -> MenuScreens.register(k, v));
+			LOGGER.info(SETUP, modMarker + " SCREENS REGISTERED ");
+			skullTextures.get().forEach((k, v) -> SkullBlockRenderer.SKIN_BY_TYPE.put(k, v));
+			LOGGER.info(SETUP, modMarker + " SKULL TEXTURES REGISTERED ");
+			fluidRenderTypes.get().forEach((k, v) -> ItemBlockRenderTypes.setRenderLayer(k, v));
+			LOGGER.info(SETUP, modMarker + " FLUID RENDER TYPES SET ");
+			SortedMap<RenderType, BufferBuilder> fixedBuffers = ObfuscationReflectionHelper.getPrivateValue(RenderBuffers.class, Minecraft.getInstance().renderBuffers(), "f_110093_");
+			renderTypes.get().forEach((k, v) -> fixedBuffers.put(k, v));
+			LOGGER.info(SETUP, modMarker + " RENDER TYPES ADDED ");
+		});
 	}
 }
--- src/main/java/com/tlregen/api/setup/MasterSetupExecutor.java
+++ src/main/java/com/tlregen/api/setup/MasterSetupExecutor.java
@@ -3,6 +3,7 @@ package com.tlregen.api.setup;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
 import java.util.function.Supplier;
 
 import org.apache.logging.log4j.LogManager;
@@ -10,18 +11,36 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
+import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.tlregen.util.TextUtil;
 
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.particle.ParticleEngine;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.npc.VillagerTrades.ItemListing;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SkullBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 
@@ -83,9 +102,81 @@ public class MasterSetupExecutor {
 		commonForgeEventListeners.genericTrades = genericTrades;
 	}
 
-	public void addToSkulls(Supplier<Map<SkullBlock.Type, ModelLayerLocation>> skullRenderers) {
+	public void addSkullModels(Supplier<Map<SkullBlock.Type, ModelLayerLocation>> skullModels) {
 		if (FMLEnvironment.dist == Dist.CLIENT) {
-			clientModEventListeners.skullRenderers = skullRenderers;
+			clientModEventListeners.skullModels = skullModels;
+		}
+	}
+
+	public void addLayerDefinitions(Supplier<Map<ModelLayerLocation, LayerDefinition>> layerDefinitions) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.layerDefinitions = layerDefinitions;
+		}
+	}
+
+	public void registerEntityRenderers(Supplier<Map<EntityType<? extends Entity>, EntityRendererProvider<Entity>>> entityRenderers) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.entityRenderers = entityRenderers;
+		}
+	}
+
+	public void registerBlockEntityRenderers(Supplier<Map<BlockEntityType<? extends BlockEntity>, BlockEntityRendererProvider<BlockEntity>>> blockEntityRenderers) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.blockEntityRenderers = blockEntityRenderers;
+		}
+	}
+
+	public void registerAdditionalModels(Supplier<List<ResourceLocation>> additionalModels) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.additionalModels = additionalModels;
+		}
+	}
+
+	public void registerColorHandlersBlock(Supplier<Map<Integer, Block>> blockColorHandlers) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.blockColorHandlers = blockColorHandlers;
+		}
+	}
+
+	public void registerDimensionSpecialEffects(Supplier<Map<ResourceLocation, DimensionSpecialEffects>> dimensionSpecialEffects) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.dimensionSpecialEffects = dimensionSpecialEffects;
+		}
+	}
+
+	public void registerParticleProvidersSprites(Supplier<Map<ParticleType<ParticleOptions>, ParticleProvider.Sprite<ParticleOptions>>> particleSprites) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.particleSprites = particleSprites;
+		}
+	}
+
+	public void registerParticleProvidersSpriteSets(Supplier<Map<ParticleType<ParticleOptions>, ParticleEngine.SpriteParticleRegistration<ParticleOptions>>> particleSpriteSets) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.particleSpriteSets = particleSpriteSets;
+		}
+	}
+
+	public void registerScreens(Supplier<Map<MenuType<? extends AbstractContainerMenu>, MenuScreens.ScreenConstructor<AbstractContainerMenu, ?>>> screens) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.screens = screens;
+		}
+	}
+
+	public void registerSkullTextures(Supplier<Map<SkullBlock.Type, ResourceLocation>> skullTextures) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.skullTextures = skullTextures;
+		}
+	}
+
+	public void setFluidRenderTypes(Supplier<Map<Fluid, RenderType>> fluidRenderTypes) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.fluidRenderTypes = fluidRenderTypes;
+		}
+	}
+
+	public void addRenderTypes(Supplier<SortedMap<RenderType, BufferBuilder>> renderTypes) {
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			clientModEventListeners.renderTypes = renderTypes;
 		}
 	}
 }
